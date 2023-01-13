6847a869479e568616cf525ec83fea7e2c464df4
Luis Antonio Barajas Ramirez <76501326+luisbarajasr@users.noreply.github.com>
2023-01-12 19:26:37 -0600

    Estableciendo Navbar
    -Titulo de la pagina
    -Icono de la pagina (falta acomodar)
    -Eliminar archivos que no sirven

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Navbar() {
+  return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'> 
+                <Link to="/" className='navbar-logo'>
+                    TRVL <i className='fa-brands fa-twitter'/>           
+                </Link>
+            </div>    
+        </nav> 
+    </>
+  )
+}
+
+export default Navbar
+
+
